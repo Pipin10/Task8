29cf054ddd6acbeed0a41a9e9f23e81104d63463
Pipin10 <61896418+Pipin10@users.noreply.github.com>
2020-06-22 10:53:21 +0700

    UP Task lab 8

---
+++ KaryawanDaily.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_8
+{
+    class KaryawanDaily : Karyawan
+    {
+        public double UpahPerJam { get; set; }
+        public double JumlahJamKerja { get; set; }
+        public override double Gaji()
+        {
+            return UpahPerJam * JumlahJamKerja;
+        }
+    }
+}
