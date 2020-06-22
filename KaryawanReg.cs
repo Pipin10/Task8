e3abf25edf890ea607bc86d85e0c7d4661cfc46b
Pipin10 <61896418+Pipin10@users.noreply.github.com>
2020-06-22 10:53:46 +0700

    UP Task lab 8

---
+++ KaryawanReg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_8
+{
+    class KaryawanReg : Karyawan
+    {
+        public double GajiBulanan { get; set; }
+        public override double Gaji()
+        {
+            return GajiBulanan;
+        }
+    }
+}
