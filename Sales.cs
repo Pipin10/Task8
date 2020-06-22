23a63abed8591f76113fdceabdefdcd5284fd166
Pipin10 <61896418+Pipin10@users.noreply.github.com>
2020-06-22 10:54:07 +0700

    UP Task lab 8

---
+++ Sales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_8
+{
+    class Sales : Karyawan
+    {
+        public double JmlPenjualan { get; set; }
+        public double Komisi { get; set; }
+        public override double Gaji()
+        {
+            return JmlPenjualan * Komisi;
+        }
+    }
+}
